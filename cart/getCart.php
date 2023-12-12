2c6194f2dbcd5fa91d96a5a7ae82a47b4645d1b1
egoRockU <lagutan.gerwin.bscs2021@gmail.com>
2023-12-12 23:28:32 +0800

    checkout done

--- cart/getCart.php
+++ cart/getCart.php
@@ -10,16 +10,20 @@ if ($_SERVER['REQUEST_METHOD']=='POST'){
     $items_array = array();
 
     $sql = "SELECT 
-    cart_items.id AS cart_id, 
-    items.id AS item_id, 
-    items.user_id AS seller_id,
-    items.name, 
-    items.details, 
-    items.price, 
-    items.image, 
-    items.stocks, 
-    cart_items.quantity
-    FROM cart_items JOIN items ON cart_items.item_id=items.id WHERE cart_items.user_id = $userId";
+            cart_items.id AS cart_id, 
+            items.id AS item_id, 
+            items.user_id AS seller_id,
+            users.username AS seller_name,
+            items.name, 
+            items.details, 
+            items.price, 
+            items.image, 
+            items.stocks, 
+            cart_items.quantity
+            FROM cart_items 
+            JOIN items ON cart_items.item_id=items.id 
+            JOIN users ON items.user_id=users.id
+            WHERE cart_items.user_id = $userId";
 
     if ($result = $conn->query($sql)){
         while($row = mysqli_fetch_assoc($result))
---
+++ cart/orders.php
@@ -0,0 +1,58 @@
+<?php 
+
+require '../connection.php';
+
+$input = json_decode(file_get_contents('php://input'));
+$userId = $input->user_id;
+$amount = $input->amount;
+$address = $input->address;
+$recipient = $input->recipient;
+$shipping = $input->shipping;
+$itemsToUpdate = $input->itemsToUpdate;
+
+// echo $userId;
+// echo $amount;
+// echo $address;
+// echo $recipient;
+// echo $shipping;
+// //print_r($itemsToUpdate);
+
+// foreach($itemsToUpdate as $item){
+//     echo $item->cart_id;
+//     echo $item->item_id;
+//     echo $item->quantity;
+// }
+
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+    
+    $sql_insert = "INSERT INTO orders (id, user_id, amount, address, recipient, shipping) 
+    VALUES (null, $userId, $amount, '$address', '$recipient', $shipping)";
+
+    if ($conn->query($sql_insert) === TRUE){
+        $response = "Order created added successfully";
+
+        foreach($itemsToUpdate as $item){
+            $cartId = $item->cart_id;
+            $itemId = $item->item_id;
+            $quantity = $item->quantity;
+
+            $sql_cart_delete = "DELETE FROM cart_items WHERE id=$cartId";
+            $sql_item_update = "UPDATE items SET stocks=stocks-$quantity WHERE id=$itemId";
+
+            if ($conn->query($sql_cart_delete) === TRUE && $conn->query($sql_item_update) === TRUE){
+                $response = $response . " Items and Cart updated";
+            } else {
+                $response = $response . " Items and Cart update Failed";
+            }
+        }
+
+    } else {
+        $response = "sql query failed";
+    }
+
+    echo $response;
+    $conn->close();
+
+}
+
+?>
